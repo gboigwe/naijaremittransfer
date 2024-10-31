c2d7d106f4394f534315904ba54a6d47ce757621
Gbolahan Akande <geakande@gmail.com>
2024-10-31 18:37:13 +0100

    Added a footer component

---
+++ clientfrontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Naija Transfer. All rights reserved.</p>
+      <p>Powered by Stacks Blockchain</p>
+    </footer>
+  );
+};
+
+export default Footer;
